3034cffdadd5502d35702ee02f0ba8d0175046b7
刘清 <liuqing2@shein.com>
2022-02-24 19:50:45 +0800

    增加添加成员功能

--- httpserv.go
+++ httpserv.go
@@ -10,10 +10,24 @@ import (
 	"github.com/kylin-ops/raft/http/httpserver/tools"
 )
 
+var (
+	UriElection         = "/api/v1/election"
+	UriHeartbeat        = "/api/v1/heartbeat"
+	UriGetInfo          = "/api/v1/get_info"
+	UriAddMemberForward = "/api/v1/add_member/forward"
+	UriDelMemberForward = "/api/v1/del_member/forward"
+	UriAddMember        = "/api/v1/add_member"
+	UriDelMember        = "/api/v1/del_member"
+)
+
 func (r *Raft) httpServer() {
-	http.HandleFunc("/api/v1/election", r.electionRequest)
-	http.HandleFunc("/api/v1/heartbeat", r.heartbeatRequest)
-	http.HandleFunc("/api/v1/get_info", r.getRaftInfo)
+	http.HandleFunc(UriElection, r.electionRequest)
+	http.HandleFunc(UriHeartbeat, r.heartbeatRequest)
+	http.HandleFunc(UriGetInfo, r.getRaftInfo)
+	http.HandleFunc(UriAddMemberForward, r.addMemberForward)
+	http.HandleFunc(UriAddMember, r.addMember)
+	http.HandleFunc(UriDelMemberForward, r.delMemberForward)
+	http.HandleFunc(UriDelMember, r.delMember)
 	if err := http.ListenAndServe(r.Address, nil); err != nil {
 		log.Fatalln(err.Error())
 	}
@@ -48,4 +62,52 @@ func (r *Raft) getRaftInfo(resp http.ResponseWriter, req *http.Request) {
 	r.Mu.Lock()
 	tools.ApiResponse(resp, 200, r, "")
 	r.Mu.Unlock()
+}
+
+func (r *Raft) addMemberForward(resp http.ResponseWriter, req *http.Request) {
+	var body map[string]string
+	data, _ := ioutil.ReadAll(req.Body)
+	_ = json.Unmarshal(data, &body)
+	resp.Header().Set("content-type", "application/json")
+	if err := r.MemberForwardLeaderResponse("add", body["id"], body["address"]); err != nil {
+		tools.ApiResponse(resp, 299, "", err.Error())
+		return
+	}
+	tools.ApiResponse(resp, 200, "", "")
+}
+
+func (r *Raft) delMemberForward(resp http.ResponseWriter, req *http.Request) {
+	var body map[string]string
+	data, _ := ioutil.ReadAll(req.Body)
+	_ = json.Unmarshal(data, &body)
+	resp.Header().Set("content-type", "application/json")
+	if err := r.MemberForwardLeaderResponse("del", body["id"], body["address"]); err != nil {
+		tools.ApiResponse(resp, 299, "", err.Error())
+		return
+	}
+	tools.ApiResponse(resp, 200, "", "")
+}
+
+func (r *Raft) addMember(resp http.ResponseWriter, req *http.Request) {
+	var body map[string]string
+	data, _ := ioutil.ReadAll(req.Body)
+	_ = json.Unmarshal(data, &body)
+	resp.Header().Set("content-type", "application/json")
+	if err := r.MemberResponse("add", body["id"], body["address"]); err != nil {
+		tools.ApiResponse(resp, 299, "", err.Error())
+		return
+	}
+	tools.ApiResponse(resp, 200, "", "")
+}
+
+func (r *Raft) delMember(resp http.ResponseWriter, req *http.Request) {
+	var body map[string]string
+	data, _ := ioutil.ReadAll(req.Body)
+	_ = json.Unmarshal(data, &body)
+	resp.Header().Set("content-type", "application/json")
+	if err := r.MemberResponse("del", body["id"], body["address"]); err != nil {
+		tools.ApiResponse(resp, 299, "", err.Error())
+		return
+	}
+	tools.ApiResponse(resp, 200, "", "")
 }
--- service.go
+++ service.go
@@ -3,13 +3,20 @@ package raft
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
+
+	"github.com/kylin-ops/raft/http/httpclient/grequest"
 )
 
 func (r *Raft) ElectionResponse(leader *Leader) error {
 	r.Mu.Lock()
 	defer r.Mu.Unlock()
+	if _, ok := r.Members[leader.LeaderId]; !ok {
+		return fmt.Errorf("响应投票请求 - %s不能参与选举，不在成员列表中",leader.LeaderId)
+	}
+
 	if r.VotedFor != "" {
 		return fmt.Errorf("响应投票请求 - %s的投票请求失败,选票已经投给%s", leader.LeaderId, r.VotedFor)
 	}
@@ -53,6 +60,65 @@ func (r *Raft) SetLeaderResponse(id string) error {
 	defer r.Mu.Unlock()
 	if _, ok := r.Members[id]; !ok {
 		return fmt.Errorf("设置leader指定的成员%s不存在", id)
+	}
+	return nil
+}
+
+// 接收添加或删除member请求的信息并转发给leader
+func (r *Raft) MemberForwardLeaderResponse(action, id, address string) error {
+	var uri string
+	r.Mu.Lock()
+	m, ok := r.Members[r.CurrentLeader]
+	if !ok {
+		r.Mu.Unlock()
+		return errors.New("集群中没有leader")
+	}
+	addr := m.Address
+	r.Mu.Unlock()
+	switch action{
+	case "add":
+		uri = UriAddMember
+	case "del":
+		uri = UriDelMember
+	default:
+		return fmt.Errorf("不支持%s操作", action)
+	}
+	url := "http://" + addr + uri
+	resp, err := grequest.Post(url, &grequest.RequestOptions{
+		Json:    true,
+		Data:    map[string]string{"id": id, "address": address},
+		Timeout: time.Second,
+	})
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode() != 200 {
+		msg, _ := resp.Text()
+		return errors.New(msg)
+	}
+	return nil
+}
+
+// 添加或删除member
+func (r *Raft) MemberResponse(action, id, address string) error {
+	r.Mu.Lock()
+	defer r.Mu.Unlock()
+	if r.Role != "leader" {
+		return errors.New("本节点不是leader不能执行添加成员")
+	}
+	switch action {
+	case "add":
+		if _, ok := r.Members[id]; ok {
+			return fmt.Errorf("成员%s已经存在", id)
+		}
+		r.Members[id] = &Member{Id: id, Address: address, LeaderId: r.CurrentLeader, Role: "follower"}
+	case "del":
+		if _, ok := r.Members[id]; !ok {
+			return fmt.Errorf("成员%s不存在", id)
+		}
+		delete(r.Members, id)
+	default:
+		return fmt.Errorf("不支持%s操作", action)
 	}
 	return nil
 }
